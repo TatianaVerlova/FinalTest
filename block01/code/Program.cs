6a2d757bc39066d8b887e14e976d248d1a92533b
Tatiana Verlova <tatiana.verlova@gmail.com>
2022-09-07 00:08:32 +0300

    add the coding part of final test

---
+++ block01/code/Program.cs
@@ -0,0 +1,23 @@
+﻿void PrintArray(string[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + "\t");
+    }
+}
+
+Console.WriteLine("Введите значения строкового массива через точку с запятой без пробела: ");
+string[] input = Console.ReadLine().Split(';');
+string res = "";
+
+for(int i = 0; i < input.Length; i++)
+{
+    int l = input[i].Length;
+    if(l <= 3)
+    {
+        res = res + input[i] + ";";
+    }
+}
+
+string[] result = res.Split(';');
+PrintArray(result);
